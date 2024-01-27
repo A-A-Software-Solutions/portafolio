7c965959b28f33b9c01331f200705346906ac3fc
Abvvs <abisanchez143@gmail.com>
2024-01-27 03:22:11 -0500

    CAMBIÉ EN SERVICIOS, CONTACTO Y PROYECTOS. EN PROYECTO DEBEMOS BUSCAR MEJORES FOTOS Y NO SABÍA QUÉ PROYECTOS AGREGAR

--- app/Views/modules/Index.php
+++ app/Views/modules/Index.php
@@ -6,7 +6,7 @@
       <div class="about-me container col-md-6">
         <div class="section-title">
           <h2>Acerca de Abigail Sánchez</h2>
-          <p>Lee mas acerca de mi</p>
+          <p>Lee más acerca de mi</p>
         </div>
         <div class="row">
           <div class="col-lg-4" data-aos="fade-right">
@@ -87,7 +87,7 @@
       <div class="about-me container col-md-6">
         <div class="section-title">
           <h2>Acerca de Alan Rodríguez</h2>
-          <p>Lee mas acerca de mi</p>
+          <p>Lee más acerca de mi</p>
         </div>
         <div class="row">
           <div class="col-lg-4" data-aos="fade-right">
@@ -119,9 +119,9 @@
               <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Disponible</span></li>
             </ul>
             <p>
-              Mi objetivo es utilizar mi pasión por la programación y los conocimientos que he adquirido, 
+              Mi objetivo es utilizar mi pasión por la programación y los conocimientos que he adquirido,
               además del aprendizaje continuo de nuevas tecnologías para crear productos digitales que
-              marquen la diferencia. Me mantengo siempre aprendiendo y experimentando con las últimas 
+              marquen la diferencia. Me mantengo siempre aprendiendo y experimentando con las últimas
               tendencias en desarrollo para ofrecer las soluciones más innovadoras.
             </p>
           </div>
@@ -169,56 +169,56 @@
   <div class="container">
 
     <div class="section-title">
-      <h2>Services</h2>
+      <h2>Servicios</h2>
       <p>Nuestros Servicios</p>
     </div>
 
     <div class="row">
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
         <div class="icon-box">
-          <div class="icon"><i class="bx bxl-dribbble"></i></div>
-          <h4><a href="">Lorem Ipsum</a></h4>
-          <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+          <div class="icon"><i class='bx bx-webcam'></i></div>
+          <h4><a href="">Desarrollo de aplicaciones web responsive</a></h4>
+          <p>Tecnologías: HTML, CSS, JavaScript, React, Angular.</p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
         <div class="icon-box">
-          <div class="icon"><i class="bx bx-file"></i></div>
-          <h4><a href="">Sed ut perspiciatis</a></h4>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+          <div class="icon"><i class='bx bxl-flutter' ></i></div>
+          <h4><a href="">Desarrollo de aplicaciones híbridas</a></h4>
+          <p>Framework: Flutter</p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
         <div class="icon-box">
-          <div class="icon"><i class="bx bx-tachometer"></i></div>
-          <h4><a href="">Magni Dolores</a></h4>
-          <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+          <div class="icon"><i class='bx bxs-data'></i></div>
+          <h4><a href="">Diseño de bases de datos y administración de información.</a></h4>
+          <p>Tecnologías: MySQL, MongoDB, Firebase, Oracle.</p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
         <div class="icon-box">
-          <div class="icon"><i class="bx bx-world"></i></div>
-          <h4><a href="">Nemo Enim</a></h4>
-          <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+          <div class="icon"><i class='bx bx-user-check' ></i></div>
+          <h4><a href="">Experiencia de usuario (UX) y diseño visual atractivo (UI).</a></h4>
+          <p>Tecnologías: Figma.</p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
         <div class="icon-box">
-          <div class="icon"><i class="bx bx-slideshow"></i></div>
-          <h4><a href="">Dele cardo</a></h4>
-          <p>Quis consequatur saepe eligendi voluptatem consequatur dolor consequuntur</p>
+          <div class="icon"><i class='bx bx-refresh' ></i></div>
+          <h4><a href="">Mantenimiento y actualizaciones.</a></h4>
+          <p></p>
         </div>
       </div>
 
       <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4">
         <div class="icon-box">
-          <div class="icon"><i class="bx bx-arch"></i></div>
-          <h4><a href="">Divera don</a></h4>
-          <p>Modi nostrum vel laborum. Porro fugit error sit minus sapiente sit aspernatur</p>
+          <div class="icon"><i class='bx bxl-nodejs' ></i></div>
+          <h4><a href="">Implementación de funciones de backend. </a></h4>
+          <p>Tecnologías: Node.js, PHP, Python, Java.</p>
         </div>
       </div>
 
@@ -231,16 +231,15 @@
   <div class="container">
 
     <div class="section-title">
-      <h2>Portfolio</h2>
-      <p>My Works</p>
+      <h2>Portafolio</h2>
+      <p>Nuestros trabajos recientes</p>
     </div>
 
     <div class="row">
       <div class="col-lg-12 d-flex justify-content-center">
         <ul id="portfolio-flters">
-          <li data-filter="*" class="filter-active">All</li>
-          <li data-filter=".filter-app">App</li>
-          <li data-filter=".filter-card">Card</li>
+          <li data-filter="*" class="filter-active">Todos</li>
+          <li data-filter=".filter-app">App móvil</li>
           <li data-filter=".filter-web">Web</li>
         </ul>
       </div>
@@ -252,8 +251,8 @@
         <div class="portfolio-wrap">
           <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
           <div class="portfolio-info">
-            <h4>App 1</h4>
-            <p>App</p>
+            <h4>TIENDA "Víveres Angelica"</h4>
+            <p>App móvil</p>
             <div class="portfolio-links">
               <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
                 title="App 1"><i class="bx bx-plus"></i></a>
@@ -266,13 +265,13 @@
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-web">
         <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">
+          <img src="assets/img/portfolio/portfolio-2.png" class="img-fluid" alt="">
           <div class="portfolio-info">
-            <h4>Web 3</h4>
-            <p>Web</p>
+            <h4>SC-CAD</h4>
+            <p>App web</p>
             <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="Web 3"><i class="bx bx-plus"></i></a>
+              <a href="assets/img/portfolio/portfolio-2.png" data-gallery="portfolioGallery" class="portfolio-lightbox"
+                title="Aplicación web para la compañía SC-CAD, topografía"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
                 class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
@@ -282,13 +281,13 @@
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-app">
         <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+          <img src="assets/img/portfolio/portfolio-3.png" class="img-fluid" alt="">
           <div class="portfolio-info">
-            <h4>App 2</h4>
-            <p>App</p>
+            <h4>MindFitDoc</h4>
+            <p>App móvil</p>
             <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="App 2"><i class="bx bx-plus"></i></a>
+              <a href="assets/img/portfolio/portfolio-3.png" data-gallery="portfolioGallery" class="portfolio-lightbox"
+                title="Aplicación desarrollada para el área de salud mental"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
                 class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
@@ -296,31 +295,17 @@
         </div>
       </div>
 
-      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-        <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
-          <div class="portfolio-info">
-            <h4>Card 2</h4>
-            <p>Card</p>
-            <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="Card 2"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
-                class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
+
 
       <div class="col-lg-4 col-md-6 portfolio-item filter-web">
         <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+          <img src="assets/img/portfolio/portfolio-5.jpeg" class="img-fluid" alt="">
           <div class="portfolio-info">
-            <h4>Web 2</h4>
-            <p>Web</p>
+            <h4>SAE</h4>
+            <p>App web</p>
             <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="Web 2"><i class="bx bx-plus"></i></a>
+              <a href="assets/img/portfolio/portfolio-5.jpeg" data-gallery="portfolioGallery" class="portfolio-lightbox"
+                title="Sistema de Acumulación de Egresos"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
                 class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
@@ -332,43 +317,11 @@
         <div class="portfolio-wrap">
           <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
           <div class="portfolio-info">
-            <h4>App 3</h4>
+            <h4>App móvil 3</h4>
             <p>App</p>
             <div class="portfolio-links">
               <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
                 title="App 3"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
-                class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-        <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">
-          <div class="portfolio-info">
-            <h4>Card 1</h4>
-            <p>Card</p>
-            <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="Card 1"><i class="bx bx-plus"></i></a>
-              <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
-                class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-        <div class="portfolio-wrap">
-          <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">
-          <div class="portfolio-info">
-            <h4>Card 3</h4>
-            <p>Card</p>
-            <div class="portfolio-links">
-              <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox"
-                title="Card 3"><i class="bx bx-plus"></i></a>
               <a href="portfolio-details.html" data-gallery="portfolioDetailsGallery" data-glightbox="type: external"
                 class="portfolio-details-lightbox" title="Portfolio Details"><i class="bx bx-link"></i></a>
             </div>
@@ -402,71 +355,52 @@
   <div class="container">
 
     <div class="section-title">
-      <h2>Contact</h2>
-      <p>Contact Me</p>
+      <h2>Contacto</h2>
+      <p>Contáctanos</p>
     </div>
 
     <div class="row mt-2">
 
       <div class="col-md-6 d-flex align-items-stretch">
         <div class="info-box">
-          <i class="bx bx-map"></i>
-          <h3>My Address</h3>
-          <p>A108 Adam Street, New York, NY 535022</p>
+          <i class="bx bx-envelope"></i>
+          <h3>Email</h3>
+          <a href="mailto: aasoftwaresolutions00@gmail.com" class="email">aasoftwaresolutions00@gmail.com</a>
         </div>
       </div>
 
       <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
         <div class="info-box">
-          <i class="bx bx-share-alt"></i>
-          <h3>Social Profiles</h3>
-          <div class="social-links">
-            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-6 mt-4 d-flex align-items-stretch">
-        <div class="info-box">
-          <i class="bx bx-envelope"></i>
-          <h3>Email Me</h3>
-          <p>contact@example.com</p>
-        </div>
-      </div>
-      <div class="col-md-6 mt-4 d-flex align-items-stretch">
-        <div class="info-box">
           <i class="bx bx-phone-call"></i>
-          <h3>Call Me</h3>
-          <p>+1 5589 55488 55</p>
+          <h3>Teléfonos:</h3>
+          <p>+593 961 535 247</p>
+          <p>+593 980 270 157</p>
         </div>
       </div>
+      
     </div>
 
     <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
       <div class="row">
         <div class="col-md-6 form-group">
-          <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+          <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" required>
         </div>
         <div class="col-md-6 form-group mt-3 mt-md-0">
-          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+          <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
         </div>
       </div>
       <div class="form-group mt-3">
-        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+        <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto" required>
       </div>
       <div class="form-group mt-3">
-        <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+        <textarea class="form-control" name="message" rows="5" placeholder="Mensaje" required></textarea>
       </div>
       <div class="my-3">
         <div class="loading">Loading</div>
         <div class="error-message"></div>
         <div class="sent-message">Your message has been sent. Thank you!</div>
       </div>
-      <div class="text-center"><button type="submit">Send Message</button></div>
+      <div class="text-center"><button type="submit">Enviar mensaje</button></div>
     </form>
 
   </div>
